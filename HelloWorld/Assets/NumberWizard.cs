23456a2216f6f567267d65f28118b26d8f0268d7
MBazarnicki <bazarep@onet.pl>
2019-10-16 09:54:41 +0200

    .

--- HelloWorld/Assets/NumberWizard.cs
+++ HelloWorld/Assets/NumberWizard.cs
@@ -7,26 +7,49 @@ public class NumberWizard : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Witaj u Liczbowego Czarodzieja");
-        print("Pomysl o jakiejs liczbie, ale nie mow jej na glos");
         int min = 1;
         int max = 1000;
-        print("Największa liczna jaka możesz wybrać to" + max);
-        print("Najmniejsza liczba jaka mozesz wybrac to" + min);
-        print("Czy twoja liczba jest mniejsza czy wieksza od 500");
-        print("Aby odpowiedzieć na pytanie nacisnij: Gorna strzalka = wyzsza Dolna strzalka + nizsza");
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        { print("Gorna strzalka zostala nacisnieta");| }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        { pirnt("Dolna strzalka zostala nacisnienta"); }
-        if (Input.GetKeyDown(KeyCode.Return))
-        { print("Enter zostal nacisniety"); }
+        int guess = 500;
+    
 
-       
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+        {
+            print("Witaj u Liczbowego Czarodzieja");
+            print("Pomysl o jakiejs liczbie, ale nie mow jej na glos");
+
+            print("Największa liczna jaka możesz wybrać to" + max);
+            print("Najmniejsza liczba jaka mozesz wybrac to" + min);
+            print("Czy twoja liczba jest mniejsza czy wieksza od 500");
+            print("Aby odpowiedzieć na pytanie nacisnij: Gorna strzalka = wyzsza Dolna strzalka = nizsza Enter=Równa");
+
+
+        }
+    
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                min = guess;
+                guess = (min + max) / 2;
+                print("Czy twoja liczba jest mniejsza czy wieksza od" + guess);
+                print("Gorna strzalka zostala nacisnieta");
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                max = guess;
+                guess = (min + max) / 2;
+                print("Czy twoja liczba jest mniejsza czy wieksza od" + guess);
+                print("Dolna strzalka zostala nacisnienta");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+
+                print("Haha zgadłem twoją liczbę");
+            }
+        }
+
+
+    
 }
